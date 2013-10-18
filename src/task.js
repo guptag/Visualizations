7e82d7fba8b37edffd1a0135df3d8aa81e734e14
Gupta Garuda <gupta.garuda@pellucid.com>
2013-10-18 19:30:28 -0400

    New interface for task (wip)

--- src/task.js
+++ src/task.js
@@ -106,7 +106,7 @@
   /*
       sample usage:
 
-      //Interface is inspired from the node module 'flow'
+      //Interface is inspired from the node module 'Flow'
 
       var task = new donut.Task();
       task.serial(function(next) {
@@ -136,85 +136,103 @@
             console.log("parallel job 1"); 
             ready();
           }
-        ]).join()
-        .serial(function(next) {
+        ]).serial(function(next) {
           console.log("job 3"); 
           next();
         }).process();
   */
-  global.donut.Task_WIP = function (finishCb) {     
-    this.onComplete = finishCb;
+
+  global.donut.Task_WIP = function () {     
     this.jobs = [];
-    this.batchJobsHash = {}; //hash for batch id and the count of functions to execute
+    this.parallelJobId = 0;    
     this.isCompleted = false;
   }
 
-  _prototype  = global.donut.Task.prototype;
-  
-  _prototype.addJob = function(p_fn) {
-    if (!this.isCompleted) {
-      this.jobs.push({
+  _prototype  = global.donut.Task_WIP.prototype;
+
+
+  _prototype.serial = function(p_fn) {
+      
+     if (this.isCompleted) {
+       console && console.log("Cannot add jobs to the completed task");
+       return this;
+     }
+      
+     this.jobs.push({
         execFn: p_fn
-      });
-    } else {
-      console && console.log("Cannot add jobs to the completed task");
+     });     
+      
+     return this;
+  };
+
+
+  _prototype.parallel = function (p_fnList) {
+
+      if (this.isCompleted) {
+        console && console.log("Cannot add jobs to the completed task");
+        return this;
+      }
+
+      //remove nulls from array
+      p_fnList = p_fnList.filter(function(e){return e}); 
+      
+      this.jobs.push({
+        meta: { 
+                  id: ++this.parallelJobId, 
+                  jobcount: p_fnList.length
+                },
+        execFnList: p_fnList
+      });  
+
+      return this;
+  };
+
+
+  _prototype.process = function () {
+      executeNextTask(this);
+      return this;
+  };  
+
+  _prototype.done = function (doneCb) {
+      this.onComplete = doneCb || function() {};
+      return this;
+  };  
+
+  var getNextCb = function(p_this) {
+    var p_this = p_this;
+    return function next() {
+      executeNextTask(p_this);
     }
   };
 
-  _prototype.addBatchJob = function(p_batch_id, p_fnList) {
-    if (!this.isCompleted) {      
-      p_fnList = p_fnList.filter(function(e){return e}); //remove nulls from array
-      this.jobs.push({
-        batchId: p_batch_id,
-        execFnList: p_fnList
-      });
+  var getReadyCb = function (p_this, meta) {
+      var meta = meta;
+      meta.completed = 0;
+      return function ready () {
+        // move to the next job once all the parallel tasks are completed
+        (++meta.completed === meta.jobcount) && executeNextTask(p_this);
+      };
+  };
 
-      // keep a hash of batch id and number of jobs it needs to execute
-      this.batchJobsHash[p_batch_id] = {total: p_fnList.length, completed: 0};
-    } else {
-      console && console.log("Cannot add work items to the completed task");
-    }
-  }  
-
-  _prototype.process = function () {
-    if (!this.isCompleted && this.jobs.length > 0) {
-      var item_to_process = this.jobs.shift();
+  var executeNextTask = function (p_this) {
+    if (!p_this.isCompleted && p_this.jobs.length > 0) {
+      var item_to_process = p_this.jobs.shift();
       
-      // if batch job
-      if(item_to_process.batchId) {
-        var _this = this;
+      // if parallel job
+      if(item_to_process.meta) {              
         $.each(item_to_process.execFnList, function(index, item) {
           // stagger calling batch jobs
           setTimeout(function() { 
-            item.call(global, _this, item_to_process.batchId); 
+            item.call(global, getReadyCb(p_this, item_to_process.meta)); 
           }, index * 25);
         })
       } else {
-        item_to_process.execFn.call(global, this);
+        item_to_process.execFn.call(global, getNextCb(p_this));
       }
     } else {
-      this.isCompleted = true;      
+      p_this.isCompleted = true;      
       // execute complete callback (if exists)
-      this.onComplete && this.onComplete.call(global);      
+      p_this.onComplete && p_this.onComplete.call(global);      
     }
-  }
-
-  _prototype.notifyJobComplete = function () {
-    if (!this.isCompleted) {
-      this.process();
-    } 
-  }
-
-  _prototype.notifyBatchItemComplete = function (p_id) {
-    if (!this.isCompleted && this.batchJobsHash[p_id]) {
-      
-      // increment completed jobs count
-      this.batchJobsHash[p_id].completed += 1;
-      
-      // if all are completed, flag the signal
-      if (this.batchJobsHash[p_id].completed  === this.batchJobsHash[p_id].total) {
-        this.notifyJobComplete();
-      }
-    } 
-  }
+  };
 })(window);  
